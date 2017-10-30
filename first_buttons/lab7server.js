e4db89827309fd08d48d72b88f4cd45466e9d9c2
Laverne Schrock <schr1230@morris.umn.edu>
2017-10-29 19:49:42 -0500

    Add all required functionality to lab7server.js

--- first_buttons/lab7server.js
+++ first_buttons/lab7server.js
@@ -2,11 +2,31 @@ var express=require('express'),
 app = express(),
 port = process.env.PORT || 1337;
 
-var buttons=[{"buttonID":1,"left":10,"top":70,"width":100,"label":"hotdogs","invID":1},{"buttonID":2,"left":110,"top":70,"width":100,"label":"hambugers","invID":2},{"buttonID":3,"left":210,"top":70,"width":100,"label":"bannanas","invID":3},{"buttonID":4,"left":10,"top":120,"width":100,"label":"milkduds","invID":4}]; //static buttons
+var mysql = require('mysql');
 
+var credentials = require('../credentials.json');
+credentials.host = "ids"; 
+
+function fetchButtons(res){
+  var connection = mysql.createConnection(credentials);
+  connection.connect(function(err) {
+	if(err) {
+		console.log("Error connecting to the database");
+	}
+  });
+  var query = 'SELECT * FROM schr1230.till_buttons';
+  connection.query(query, function(err,rows,fields) {
+	if(err) {
+		console.log("Error connecting to the database");
+	} else {
+		res.send(rows);
+	}
+  });
+  connection.end();
+}
 app.use(express.static(__dirname + '/public')); //Serves the web pages
 app.get("/buttons",function(req,res){ // handles the /buttons API
-  res.send(buttons);
+  fetchButtons(res);
 });
 
 app.listen(port);
